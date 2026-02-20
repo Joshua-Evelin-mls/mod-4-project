1b9ba2685440cf5e9c5bd23af9a79a2883acce18
JoshuaDesrosiers <desrosiersjoshua3@gmail.com>
2026-02-20 11:56:02 -0500

    initialized tailwind3

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app.html","./index.html","./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
