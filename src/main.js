962812bda66898319705da1c5996abe902a32b75
JoshuaDesrosiers <desrosiersjoshua3@gmail.com>
2026-02-23 13:56:34 -0500

    added quota feature and proper stylings

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './vanilla.css'
 import 'material-icons/iconfont/material-icons.css';
 import { collection,temp, populatePopUp, renderCards } from './domHelpers.js'
 
+let _points = Number(localStorage.getItem('points'))||localStorage.setItem('points',1)||Number(localStorage.getItem('points'))
+const dateLastGifted = localStorage.getItem('giftDate')||0
+
+
 if (collection.length > 0) {
   renderCards()
 }
@@ -28,11 +32,30 @@ next.addEventListener('click',()=>{
     for(let elem of chosen){
       collection.push(temp[elem.dataset.id])
     }
+    localStorage.setItem('points',_points-chosen.length)
+    _points-=chosen.length
+    points.innerText = ' '+_points
     localStorage.setItem('collection',JSON.stringify({data:structuredClone(collection)}))
 
     app.innerHTML = ''
     renderCards()
 })
+
+const daily = document.getElementById('daily')
+
+const today = new Date();
+const formattedDate = String(today.toLocaleDateString('en-GB'));
+console.log(formattedDate)
+dateLastGifted==formattedDate?daily.setAttribute('disabled',true):''
+daily.addEventListener('click', async ()=>{
+  console.log(dateLastGifted)
+    localStorage.setItem('giftDate',String(formattedDate))
+    daily.setAttribute('disabled',true)
+    localStorage.setItem('points',_points+5)
+    _points+=5
+    points.innerText = ' '+_points
+})
+
 
 const inject = document.getElementById('inject')
 inject.addEventListener('click',(e)=>{
@@ -44,9 +67,12 @@ inject.addEventListener('click',(e)=>{
     console.log('hi')
     _.classList.remove('chosen')
     _.classList.add('unchosen')
-  } else if (document.querySelectorAll('.chosen').length<5) {
+  } else if (document.querySelectorAll('.chosen').length<_points) {
     _.classList.remove('unchosen')
     _.classList.add('chosen')
   }
 }
 })
+
+const points = document.getElementById('points')
+points.innerText = ' '+_points
